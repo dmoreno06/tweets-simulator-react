a4dbe8e8614bac7eaa66cc864fbb690df8813c42
jsrodriguezm <jsrodriguezm@indracompany.com>
2023-04-10 16:39:43 -0500

    header project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from "react";
+import Header from "./components/Header";
+import {Container} from "@mui/material";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container className='tweets-simulator' maxWidth={false}>
+    <Header/>
+    </Container>
   );
 }
 
